60b4635b3a16f210467b22cd041ad2f5b5f1e3de
RahulG5744 <rahulghatul203@gmail.com>
2025-11-09 23:38:21 +0530

    Add files via upload
    Initial commit: Added Find Maximum Number in Array project using JavaScript

---
+++ script.js
@@ -0,0 +1,8 @@
+// Program to find the maximum number in an array
+
+const numbers = [12, 45, 67, 23, 89, 34];
+
+const maxNum = Math.max(...numbers);
+
+console.log("The numbers are:", numbers);
+console.log("The largest number is:", maxNum);
